ba4791cd33c718dba96b7a8299ca1ba9cf5b1478
dpid <dlux@dluxgames.com>
2018-01-03 07:30:43 -0800

    First commit

---
+++ Assets/Scripts/CubotController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CubotController : MonoBehaviour {
+	
+	void FixedUpdate () {
+        float torque = Input.GetAxis("Horizontal") * 0.1f;
+        float force = Input.GetAxis("Vertical") * 1.0f;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        Transform transform = GetComponent<Transform>();
+
+        rigidbody.AddTorque(transform.up * torque);
+        rigidbody.AddForce(transform.forward * force);
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Unity Emissive Lighting
+
+This Unity project shows some example scenes of how to use emissive lighting with real time global illumination.
+
+Unity Version
+--------
+[Unity 2017.3.0](https://unity3d.com/get-unity/download/archive "Unity - Download Archive")
